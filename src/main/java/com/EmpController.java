4b124bcee9287a73f07b3355e30ffd602860333f
Swetasm <swetasm16@gmail.com>
2023-03-16 11:43:40 -0400

    second file pushed from eclipse

---
+++ src/main/java/com/EmpController.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmpController {
+	
+	
+@GetMapping("/hello")
+String giveData() {
+	return "Hiiiiiiiii";
+}
+}
+
